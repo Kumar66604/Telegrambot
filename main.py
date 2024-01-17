281f563d91b58423961069d190140a14281c4317
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-01-17 17:34:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,18 +28,18 @@ async def toggle_approval(client, message):
     global approve_mode
     approve_mode = not approve_mode  # Toggle the boolean value
 
-    await message.reply(f"Join request approval is now {'on' if approve_mode else 'off'>
+    await message.reply(f"Join request approval is now {'on' if approve_mode else 'off'}.")
 
 @app.on_chat_join_request()
 async def handle_join_request(client, chat_join_request):
     if approve_mode:
         try:
-            await client.approve_chat_join_request(chat_join_request.chat.id, chat_join>
+            await client.approve_chat_join_request(chat_join_request.chat.id, chat_join_request.from_user.id)
 
         except Exception as e:
-            await client.send_message(chat_join_request.chat.id, f"Failed to approve jo>
+            await client.send_message(chat_join_request.chat.id, f"Failed to approve join request: {e}")
     else:
-        await client.send_message(chat_join_request.chat.id, "Join request pending manu>
+        await client.send_message(chat_join_request.chat.id, "Join request pending manual approval.")
 
 
 print("BOT SUCCESSFULLY DEPLOYED !!")
