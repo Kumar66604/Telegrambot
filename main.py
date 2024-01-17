f50894462182b691245cc295a37ebb65c220fcbb
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-01-17 17:32:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ for file in os.listdir():
 for file in os.listdir():
     if file.endswith(".session-journal"):
         os.remove(file)
-        
+       
+
 app = Client(
     "Test-Bot",
     api_id=API_ID,
@@ -19,6 +20,27 @@ app = Client(
 @app.on_message(filters.command("start"))
 async def start(_, message):
     await message.reply_text("Hello! I am your bot. How can I help you?")
+
+approve_mode = False
+
+@app.on_message(filters.command("approvejoin") & filters.private)
+async def toggle_approval(client, message):
+    global approve_mode
+    approve_mode = not approve_mode  # Toggle the boolean value
+
+    await message.reply(f"Join request approval is now {'on' if approve_mode else 'off'>
+
+@app.on_chat_join_request()
+async def handle_join_request(client, chat_join_request):
+    if approve_mode:
+        try:
+            await client.approve_chat_join_request(chat_join_request.chat.id, chat_join>
+
+        except Exception as e:
+            await client.send_message(chat_join_request.chat.id, f"Failed to approve jo>
+    else:
+        await client.send_message(chat_join_request.chat.id, "Join request pending manu>
+
 
 print("BOT SUCCESSFULLY DEPLOYED !!")
 
